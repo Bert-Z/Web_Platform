af9a66ff5089272cac5d2efcb68e5bb29116ec57
Bert-Z <wanqiangzhang@sjtu.edu.cn>
2019-03-11 14:43:26 +0800

    Finish Calculate Class
    Finish Calculate Class

--- hw1/calculator/src/main/java/calculator/Calculate.java
+++ hw1/calculator/src/main/java/calculator/Calculate.java
@@ -17,8 +17,139 @@ public class Calculate {
         return n * factorial(n - 1);
     }
 
-//    private double basic(){
-//
-//    }
+    private double basic() {
+        Token t = ts.get();
+        switch (t.kind) {
+            case ('('): {
+                double d = expression();
+                t = ts.get();
+                if (t.kind != ')') {
+                    throw new RuntimeException("')'excepted");
+                }
+
+                return d;
+            }
+            case NUMBER:
+                return t.value;
+            default:
+                throw new RuntimeException("basic expected");
+        }
+    }
+
+    private double primary() {
+        double left = basic();
+        Token t = ts.get();
+        if (t.kind == '!') {
+            if ((int) (left) == left && left >= 0) {
+                return factorial((int) left);
+            } else {
+                throw new RuntimeException("error");
+            }
+
+        } else {
+            ts.putback(t);
+        }
+
+        return left;
+    }
+
+
+    private double mid() {
+        Token t = ts.get();
+        switch (t.kind) {
+            case '-':
+                return -mid();
+            case '+':
+                return +mid();
+            default:
+                ts.putback(t);
+                return primary();
+        }
+    }
+
+
+    private double term() {
+        double left = mid();
+        Token t = ts.get();
+
+        while (true) {
+            switch (t.kind) {
+                case '*': {
+                    left *= mid();
+                    t = ts.get();
+                    break;
+                }
+                case '/': {
+                    double d = mid();
+                    if (d == 0)
+                        throw new RuntimeException("divide by zero");
+                    left /= d;
+                    t = ts.get();
+                    break;
+                }
+                case '%': {
+                    double d = mid();
+                    int i1 = (int) left;
+                    if (i1 != left)
+                        throw new RuntimeException("error");
+                    int i2 = (int) d;
+                    if (i2 != d)
+                        throw new RuntimeException("error");
+                    if (d == 0)
+                        throw new RuntimeException("%:divided by zero");
+                    left = i1 % i2;
+                    t = ts.get();
+                    break;
+                }
+                default: {
+                    ts.putback(t);
+                    return left;
+                }
+            }
+        }
+    }
+
+
+    private double expression() {
+        double left = term();
+        Token t = ts.get();
+
+        while (true) {
+            switch (t.kind) {
+                case '+': {
+                    left += term();
+                    t = ts.get();
+                    break;
+                }
+                case '-': {
+                    left -= term();
+                    t = ts.get();
+                    break;
+                }
+                default: {
+                    ts.putback(t);
+                    return left;
+                }
+            }
+        }
+    }
+
+    public void calculate() {
+        System.out.print(PROMPT);
+        Token t = ts.get();
+
+        while (t.kind == PRINT)
+            t = ts.get();
+
+        if (t.kind == QUIT) {
+            System.exit(0);
+        }
+        ts.putback(t);
+        double ans = expression();
+        System.out.print(RESULT + ans + "/n");
+
+        ansvalue = ans;
+    }
+
 
 }
--- hw1/calculator/src/main/java/calculator/TokenStream.java
+++ hw1/calculator/src/main/java/calculator/TokenStream.java
@@ -1,5 +1,10 @@
 package calculator;
 
+import java.io.*;
+import java.util.Scanner;
+import java.io.PushbackInputStream;
+import java.io.ByteArrayInputStream;
+
 public class TokenStream {
     public boolean full;
     public Token buffer;
@@ -9,18 +14,41 @@ public class TokenStream {
         this.full = false;
     }
 
-    public void pushback(Token t) {
+    public void putback(Token t) {
         this.buffer = t;
         full = true;
     }
 
-//    public Token get() {
-//        if (this.full) {
-//            this.full = false;
-//            return buffer;
+    //unfinished
+    public Token get() {
+        if (this.full) {
+            this.full = false;
+            return buffer;
+        }
+
+        char ch = 'a';
+        Scanner scan = new Scanner(System.in);
+
+        Token a = new Token(ch);
+        return a;
+    }
+
+//    public static void main(String[] args) {
+//        try {
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println(scan.next());
+//        String a = scan.next();
+//        if (a == "qa") {
+//            System.out.println(a);
+//            System.exit(1);
 //        }
 //
-//
-//
+//        System.out.println('a');
+//        System.exit(2);
+//                throw new RuntimeException("')'excepted");
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
 //    }
+
 }
